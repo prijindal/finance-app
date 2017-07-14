85ebd72c4ceb835563c224ea50007c86cdd02d8e
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-07-14 14:22:19 +0530

    Modified Electron Build

--- app/electron-build.sh
+++ app/electron-build.sh
@@ -1,5 +1,7 @@
-# yarn run build
+yarn run build
 
-# ./node_modules/.bin/electron-packager . FinanceApp --platform=win32 --arch=x64
-# ./node_modules/.bin/electron-packager . FinanceApp --platform=win32 --arch=ia32
+./node_modules/.bin/electron-packager . FinanceApp --platform=win32 --arch=x64
+./node_modules/.bin/electron-packager . FinanceApp --platform=win32 --arch=ia32
 ./node_modules/.bin/electron-packager . FinanceApp --platform=linux --arch=x64
+
+rm -r FinanceApp-*/resources/app/node_modules
